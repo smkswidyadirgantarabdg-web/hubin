018b4dc05c55bedc458a6a74b426b7bd98f8f3b8
smkswidyadirgantarabdg-web <smkswidyadirgantarabdg@gmail.com>
2026-09-20 20:40:35 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.HUBIN_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxBvI5Go0HxDMpC_988mmihnRAjPzx9i8LErXioTrYEsmxbH_90UiOMAZRajA_8dccc/exec",
-  SCHOOL_NAME: "SMKS WIDYA DIRGANTARA BANDUNG",
+  SCHOOL_NAME: "SMK WIDYA DIRGANTARA",
   SCHOOL_SHORT: "SMKWD",
   SESSION_KEY: "hubin_session_v1",
   CACHE_TTL_MS: 5 * 60 * 1000
