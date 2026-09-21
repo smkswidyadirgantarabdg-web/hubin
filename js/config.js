79449866f14a2f859f3a148d600d9db341e6c2e6
smkswidyadirgantarabdg-web <smkswidyadirgantarabdg@gmail.com>
2026-09-21 20:38:44 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-window.HUBIN_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxBvI5Go0HxDMpC_988mmihnRAjPzx9i8LErXioTrYEsmxbH_90UiOMAZRajA_8dccc/exec",
-  SCHOOL_NAME: "SMK WIDYA DIRGANTARA",
-  SCHOOL_SHORT: "SMKWD",
-  SESSION_KEY: "hubin_session_v1",
-  CACHE_TTL_MS: 5 * 60 * 1000
-};
